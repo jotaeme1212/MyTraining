369e8ca54cbdd1725137356a91663786ac3a0589
JMM <juan.manuel.marchese@gmail.com>
2011-10-31 22:48:51 -0300

    Primeros Pasos 3.

--- MyTraining/ViewController.m
+++ MyTraining/ViewController.m
@@ -24,7 +24,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib
+	// Do any additional setup after loading the view, typically from a nib.
 
     // Recupera el archivo de idioma para armar el diccionario de mensajes.
     //NSDictionary *diccTextos = [AppConfiguracion obtenerIdioma:@"en"]; // Idioma forzado
